75d22fad8dbe9b8ae854da1e283703c54a9cd8fb
josephlopez04 <136289798+josephlopez04@users.noreply.github.com>
2025-06-06 23:30:08 -0700

    Create Admin_proceso.ps1

---
+++ Admin_proceso.ps1
@@ -0,0 +1,276 @@
+PS C:\Users\Jose Manuel> Get-Process
+
+ NPM(K)    PM(M)      WS(M)     CPU(s)      Id  SI ProcessName
+ ------    -----      -----     ------      --  -- -----------
+      7     1.60       6.32       0.14    8204   0 AggregatorHost
+     20     9.62      25.10       0.34   13380   6 ApplicationFrameHost
+     13     2.46      11.20      10.28   11288   6 atieclxx
+      8     1.70       5.92       0.20    2308   0 atiesrxx
+     41   304.39     177.22     254.31    2548   6 brave
+     19    13.16      29.19       0.09    3608   6 brave
+     66   182.58     239.77     245.70    3992   6 brave
+     24    87.93     100.87      86.02    6128   6 brave
+     23    43.42      86.28      13.89    8152   6 brave
+     34   123.22     121.49      98.00    9076   6 brave
+     15     9.71      20.50       0.48    9080   6 brave
+     17     7.85      22.12       0.20    9336   6 brave
+     31    98.01     143.48     155.06   11052   6 brave
+     25    61.21      77.21       5.77   11776   6 brave
+     26    69.34      99.91      10.06   12872   6 brave
+      9     2.13       7.62       0.08   12880   6 brave
+     21    18.16      38.12       0.38   13648   6 brave
+     25    15.65      41.02      29.36   15336   6 brave
+     46   189.80     219.40     141.23   15748   6 brave
+     21    17.76      45.25       2.67   16020   6 brave
+     10     1.59       1.15       0.19    9928   0 BraveCrashHandler
+      9     1.62       0.79       0.09    9936   0 BraveCrashHandler64
+     35   100.93      79.86       1.88    1196   6 Code
+     25    68.41      78.50       0.91    5204   6 Code
+     33   170.48     166.34      57.92    6184   6 Code
+     12    10.75      30.97       0.05    6564   6 Code
+     27   168.53      62.16      34.03    8168   6 Code
+     19    13.88      44.80       0.67   10380   6 Code
+     52    96.46      91.91       9.80   13748   6 Code
+     26    84.74      99.71       9.50   13840   6 Code
+     27   116.38      82.77       4.55   15192   6 Code
+      7     1.61       8.38       0.14   14104   6 CompPkgSrv
+      8     1.29       6.67       0.05    4092   6 conhost
+      9     6.38       7.18       3.08    5628   0 conhost
+     14    11.39      23.22       9.77    6696   6 conhost
+      8     1.28       6.67       0.03    9856   6 conhost
+     26     2.05       6.11       4.38     772   0 csrss
+     24     5.79       6.88      20.27    6392   6 csrss
+     18     4.84      21.22      12.77    8884   6 ctfmon
+     11     2.35      11.35       0.06    1100   6 DAX3API
+     12     3.91       9.50       1.38    4188   0 DAX3API
+     17     3.73      11.18       0.25    7464   0 dllhost
+     24     6.18      14.85       0.44   13676   6 dllhost
+     42   105.30      61.93     391.98    9468   6 dwm
+    121   138.80     155.56      74.27   14324   6 explorer
+      9     1.56       6.79       0.25    4196   0 FMService64
+      7     1.76       3.60       0.09    1088   0 fontdrvhost
+      8     2.52       6.41       0.36   12240   6 fontdrvhost
+     40    68.52      82.54       7.12    9400   6 GitHubDesktop
+     26   130.16      82.41       3.69   11968   6 GitHubDesktop
+     18    13.63      43.22       0.73   12864   6 GitHubDesktop
+     55    84.35      94.05      12.62   13548   6 GitHubDesktop
+     16     3.75      16.43       4.25    4888   0 hamachi-2
+      0     0.06       0.01       0.00       0   0 Idle
+     22    21.78      21.47       2.08    4456   0 LCD_Service
+      9     1.95       7.50       0.55    4448   0 LMIGuardianSvc
+     31    26.03      41.23       0.81   16344   6 LockApp
+     29    10.76      24.87      48.48     516   0 lsass
+     25     7.47      18.36       1.50    4468   0 MateBookService
+     57   110.21      50.76       5.17   15204   6 MBAMessageCenter
+      0     1.25     433.25     212.50    2872   0 Memory Compression
+     22    31.84      27.80      56.39    8684   0 MoUsoCoreWorker
+     17    12.19      18.38       3.88   13288   0 MpDefenderCoreService
+     25   187.30     185.26      45.66     764   6 msedge
+     18    13.18      33.76       0.95    8124   6 msedge
+     19    19.29      41.78       2.22    8232   6 msedge
+     22    44.51      83.71      23.38    9816   6 msedge
+     59    63.15     149.19      61.91   11140   6 msedge
+      9     2.10       7.82       0.02   13036   6 msedge
+     32   377.04     113.94      45.59   13388   6 msedge
+     11     7.75      17.10       0.14   13564   6 msedge
+     17    15.04      23.15       0.23   14200   6 msedge
+     14     8.11      23.82       0.20   15752   6 msedge
+    220   342.08     235.39     530.70   13508   0 MsMpEng
+     13     3.04       0.93       0.22   13880   6 MusNotifyIcon
+     12    33.02       6.01       0.12    4640   0 mysqld
+    401   691.07      62.61      69.95    5520   0 mysqld
+     13     8.31      14.26      17.97    9324   0 NisSrv
+     13     3.06      14.75       0.17    2040   6 notepad
+     28    45.41      40.40      16.39    4240   0 OfficeClickToRun
+     14     2.86      13.04       0.08    8896   0 osdservice
+     95    59.75     141.31       1.92    4008   6 PhoneExperienceHost
+     96    44.86     122.02      11.97   10304   6 pwsh
+    126    54.81     116.30      15.47   11248   6 pwsh
+     78    31.34      78.83       4.52   12192   6 pwsh
+     14    10.49      76.34       2.83     124   0 Registry
+     12     2.92       9.26       0.47    4576   0 RtkAudUService64
+     10     1.88       8.54       0.03   11152   6 RtkAudUService64
+      9     2.62       6.59       1.86    4564   0 RtkBtManServ
+     25    10.35      36.91       2.56    2956   6 RuntimeBroker
+     17     5.44      17.18       0.59    5584   6 RuntimeBroker
+     35    35.47      68.79       6.25    7016   6 RuntimeBroker
+     18     6.43      27.08       6.66    8176   6 RuntimeBroker
+     24     9.87      33.07       3.14   13332   6 RuntimeBroker
+    129   192.20     104.51      20.52    8288   6 SearchApp
+      8     1.44       8.00       0.02    1724   0 SearchFilterHost
+     74    27.46      33.02       0.83    9688   0 SearchIndexer
+     12     2.40      13.91       0.11    9480   0 SearchProtocolHost
+      9     1.73       9.11       0.08   13516   6 SearchProtocolHost
+     16     6.27      13.38       4.84   10368   0 SecurityHealthService
+     12     7.14      11.86      52.72     408   0 services
+      4     0.74       2.96       0.08    4556   0 SessionService
+     33    39.35      46.52       6.75   11284   6 ShellExperienceHost
+     19     7.01      27.98       5.45    8668   6 sihost
+     24     8.12      23.71       0.20    8820   6 smartscreen
+      4     1.05       1.13       0.30     472   0 smss
+     20     5.35      16.23       0.23    2996   0 spoolsv
+     31    39.19      45.31       5.48    1008   0 sqlceip
+     31    39.82      41.52       4.34   14076   0 sqlceip
+     79   516.18     151.12   4,281.67    3216   0 sqlservr
+     55   414.16     130.89   3,900.58   10508   0 sqlservr
+     10     2.05       7.27       0.16    4616   0 sqlwriter
+     34    39.70      60.10       2.34    5824   6 StartMenuExperienceHost
+     93    56.57      61.24      10.56    2908   6 steam
+     13     6.20      12.68       0.78   12848   0 steamservice
+     14     8.52      17.63       0.08    2372   6 steamwebhelper
+     20    12.81      27.43      10.20    4072   6 steamwebhelper
+     12     7.91      11.45       0.08    5008   6 steamwebhelper
+     29   320.60     176.59      71.55    6012   6 steamwebhelper
+     44    48.06     146.42      36.78    7536   6 steamwebhelper
+     13    11.62      16.62       0.17   10628   6 steamwebhelper
+     43   251.36      65.90     131.83   11080   6 steamwebhelper
+     27    88.58      66.72      89.47   13660   6 steamwebhelper
+      8     1.58       6.74       0.16     884   0 svchost
+     25    16.48      35.11      74.72    1068   0 svchost
+     19    12.65      17.71     115.94    1248   0 svchost
+     10     2.95       8.45       4.75    1308   0 svchost
+     13     3.41      12.11       5.22    1360   0 svchost
+     21     2.01       8.22       0.80    1432   0 svchost
+     16     1.92      10.98       0.23    1440   0 svchost
+     19     2.77      11.04       0.83    1452   0 svchost
+     13     2.65       9.85       1.62    1568   0 svchost
+      9     2.20      11.78       0.94    1576   0 svchost
+     14    15.50      14.21       8.03    1680   0 svchost
+     11     2.69       8.19       0.11    1752   0 svchost
+     11     1.88       8.46       0.06    1772   0 svchost
+     26     6.50       9.64       6.61    1872   0 svchost
+     12     2.41      10.63       0.25    1912   0 svchost
+     10     2.61       7.51       8.28    2032   0 svchost
+     14     3.93      10.78       1.34    2088   0 svchost
+      9     1.66       7.06       0.16    2268   0 svchost
+     19     7.10      15.75      23.50    2288   0 svchost
+      8     1.27       6.11       0.03    2296   0 svchost
+     11     3.40      11.19      17.25    2324   0 svchost
+     16     5.37      12.30      12.22    2352   0 svchost
+     12     2.88      11.39      18.39    2432   0 svchost
+      9     1.98       7.28       0.36    2464   0 svchost
+     10     1.87       7.96       1.75    2496   0 svchost
+     10     9.77      16.89      93.19    2508   0 svchost
+      7     1.96       6.11       2.08    2644   0 svchost
+     14     3.77      10.10      12.80    2664   0 svchost
+     12     3.02      13.90     331.06    2716   0 svchost
+      9     1.98       7.60       0.88    2732   0 svchost
+      8     1.41       5.93       1.05    2740   0 svchost
+     10     2.28       7.57       7.19    2820   0 svchost
+     13     2.04       7.67       0.94    2928   0 svchost
+      8     1.58       6.78       0.11    3012   0 svchost
+      9     1.98       7.59       1.30    3020   0 svchost
+     10     2.23       8.48       0.56    3028   0 svchost
+     15     3.82       8.88      55.27    3204   0 svchost
+     14     3.85      13.72       5.52    3352   0 svchost
+     15     2.93       9.41       4.02    3472   0 svchost
+     15     2.67      10.29       2.94    3480   0 svchost
+     25     7.08      17.32       9.17    3620   0 svchost
+     15     2.73      13.94       1.09    3672   0 svchost
+     16     5.52      13.88       1.92    3824   0 svchost
+     32    11.19      13.56      27.92    3848   0 svchost
+     11     2.16       7.76       1.53    3932   0 svchost
+      8     1.22       5.78       0.06    4160   0 svchost
+     28    24.40      33.86      26.50    4176   0 svchost
+     26     4.51      13.27       6.19    4220   0 svchost
+     22    37.66      31.89     131.19    4272   0 svchost
+     14     3.05       7.81      12.42    4296   0 svchost
+     14     9.79      16.92      17.86    4320   0 svchost
+
+PS C:\Users\Jose Manuel> Get-Process -Name AggregatorHost
+
+ NPM(K)    PM(M)      WS(M)     CPU(s)      Id  SI ProcessName
+ ------    -----      -----     ------      --  -- -----------
+      7     1.65       6.35       0.14    8204   0 AggregatorHost
+
+PS C:\Users\Jose Manuel> Get-Process -Name Search*
+
+ NPM(K)    PM(M)      WS(M)     CPU(s)      Id  SI ProcessName
+ ------    -----      -----     ------      --  -- -----------
+    129   192.20     104.51      20.52    8288   6 SearchApp
+      9     1.63       8.05       0.05    2112   0 SearchFilterHost
+     71    26.64      32.43       0.84    9688   0 SearchIndexer
+     10     1.84       9.16       0.08   13516   6 SearchProtocolHost
+
+PS C:\Users\Jose Manuel> Get-Process -id 1120
+NPM(K)    PM(M)      WS(M)     CPU(s)      Id  SI ProcessName
+ ------    -----      -----     ------      --  -- -----------
+      9     2.79       5.99       0.95    1120   0 WUDFHost
+
+PS C:\Users\Jose Manuel> Get-Process WUDFHost -FileVersionInfo
+
+ProductVersion   FileVersion      FileName
+--------------   -----------      --------
+10.0.19041.1     10.0.19041.1 (W… C:\Windows\System32\WUDFHost.exe
+
+PS C:\Users\Jose Manuel> Get-Process WUDFHost -IncludeUserName
+
+     WS(M)   CPU(s)      Id UserName                       ProcessName
+     -----   ------      -- --------                       -----------
+      6.04     0.95    1120 NT AUTHORITY\LOCAL SERVICE     WUDFHost
+
+PS C:\Users\Jose Manuel> Get-Process WUDFHost -Module
+
+   Size(K) ModuleName                                         FileName
+   ------- ----------                                         --------
+       280 WUDFHost.exe                                       C:\Windows\System32\WUDFHost.exe
+      2016 ntdll.dll                                          C:\WINDOWS\SYSTEM32\ntdll.dll
+       776 KERNEL32.DLL                                       C:\WINDOWS\System32\KERNEL32.DLL
+      3068 KERNELBASE.dll                                     C:\WINDOWS\System32\KERNELBASE.dll
+      1164 RPCRT4.dll                                         C:\WINDOWS\System32\RPCRT4.dll
+      3412 combase.dll                                        C:\WINDOWS\System32\combase.dll
+      1024 ucrtbase.dll                                       C:\WINDOWS\System32\ucrtbase.dll
+       636 sechost.dll                                        C:\WINDOWS\System32\sechost.dll
+       156 bcrypt.dll                                         C:\WINDOWS\System32\bcrypt.dll
+       200 WUDFPlatform.dll                                   C:\WINDOWS\SYSTEM32\WUDFPlatform.dll
+       204 DEVOBJ.dll                                         C:\WINDOWS\SYSTEM32\DEVOBJ.dll
+       708 advapi32.dll                                       C:\WINDOWS\System32\advapi32.dll
+       312 cfgmgr32.dll                                       C:\WINDOWS\System32\cfgmgr32.dll
+       200 SspiCli.dll                                        C:\WINDOWS\SYSTEM32\SspiCli.dll
+       632 msvcrt.dll                                         C:\WINDOWS\System32\msvcrt.dll
+        72 kernel.appcore.dll                                 C:\WINDOWS\SYSTEM32\kernel.appcore.dll
+       520 bcryptPrimitives.dll                               C:\WINDOWS\System32\bcryptPrimitives.dll
+      3400 gfspi.dll                                          c:\windows\system32\drivers\umdf\gfspi.dll
+      1392 CRYPT32.dll                                        C:\WINDOWS\System32\CRYPT32.dll
+        96 cryptsp.dll                                        C:\WINDOWS\SYSTEM32\cryptsp.dll
+       300 powrprof.dll                                       C:\WINDOWS\SYSTEM32\powrprof.dll
+        72 UMPDC.dll                                          C:\Windows\System32\UMPDC.dll
+      1936 dbghelp.dll                                        C:\Windows\System32\dbghelp.dll
+       748 WUDFx02000.dll                                     C:\Windows\System32\WUDFx02000.dll
+        40 DPAPI.dll                                          C:\Windows\System32\DPAPI.dll
+        48 CRYPTBASE.dll                                      C:\Windows\System32\CRYPTBASE.dll
+       208 rsaenh.dll                                         C:\WINDOWS\system32\rsaenh.dll
+
+PS C:\Users\Jose Manuel> Stop-Process -Name WUDFHost -Confirm -PassThru
+
+Confirm
+Are you sure you want to perform this action?
+Performing the operation "Stop-Process" on target "WUDFHost (1120)".
+[Y] Yes  [A] Yes to All  [N] No  [L] No to All  [S] Suspend  [?] Help (default is "Y"): Y
+
+Confirm
+Are you sure you want to perform the Stop-Process operation on the following item: WUDFHost(1120)?
+[Y] Yes  [A] Yes to All  [N] No  [L] No to All  [S] Suspend  [?] Help (default is "Y"): Y
+
+ NPM(K)    PM(M)      WS(M)     CPU(s)      Id  SI ProcessName
+ ------    -----      -----     ------      --  -- -----------
+      9     2.79       6.07       0.97    1120   0 WUDFHost
+
+PS C:\Users\Jose Manuel> Start-Process -file "D:\Documents\FIC\Q6\ASO\TT\TT.txt" -Verb Print -PassThru
+Handle  NPM(K)    PM(K)      WS(K)     CPU(s)      Id  SI ProcessName
+------  ------    -----      -----     ------      --  -- -----------
+    34       7     2.79       6.07       0.02    13784 10 notepad
+
+PS C:\Users\Jose Manuel> Get-Process -Name notep*
+andle  NPM(K)    PM(K)      WS(K)     CPU(s)      Id  SI ProcessName
+------  ------    -----      -----     ------     --  -- -----------
+   263      16     3812      15912       0.06   6208  10 notepad
+
+PS C:\Users\Jose Manuel> Wait-Process -Name notepad
+
+PS C:\Users\Jose Manuel> Get-Process -Name notep*
+ndle  NPM(K)    PM(K)      WS(K)     CPU(s)      Id  SI ProcessName
+------  ------    -----      -----     ------    --  -- -----------
+   263      16     3848      15908       0.08 11568  10 notepad
+
+PS C:\Users\Jose Manuel> Get-Process -Name notepad | Wait-Process
