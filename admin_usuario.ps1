488e96842de1993f9dee66f6584a3b7278ecd94a
josephlopez04 <136289798+josephlopez04@users.noreply.github.com>
2025-06-06 23:31:02 -0700

    Create admin_usuario.ps1

---
+++ admin_usuario.ps1
@@ -0,0 +1,10 @@
+PS C:\Users\Jose Manuel> Get-LocalUser
+
+Name               Enabled Description
+----               ------- -----------
+Administrador      False   Cuenta integrada para la administración del equipo o dominio
+DefaultAccount     False   Cuenta de usuario administrada por el sistema.
+Invitado           False   Cuenta integrada para el acceso como invitado al equipo o dominio
+Jose Manuel        True
+WDAGUtilityAccount False   Una cuenta de usuario que el sistema administra y usa para escenarios de Protección de aplicaciones
+
